4424180801d39519ab1df2d2ae874b7bcd082ecb
Karsten Jeschkies <k@jeschkies.xyz>
2022-10-24 10:58:45 +0200

    Import am2320 driver.

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,43 @@
-use tokio;
-
+use am2320::{Am2320, Error};
 use hap::{
-    accessory::{temperature_sensor::TemperatureSensorAccessory, AccessoryCategory, AccessoryInformation},
-    service::temperature_sensor::TemperatureSensorService,
+    accessory::{
+        temperature_sensor::TemperatureSensorAccessory, AccessoryCategory, AccessoryInformation,
+    },
+    characteristic::{
+        current_temperature::CurrentTemperatureCharacteristic, CharacteristicCallbacks,
+    },
     server::{IpServer, Server},
+    service::temperature_sensor::TemperatureSensorService,
     storage::{FileStorage, Storage},
-    Config,
-    MacAddress,
-    Pin,
-    Result,
+    Config, MacAddress, Pin, Result,
 };
+use rppal::{hal::Delay, i2c::I2c};
+use tokio;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let mut temperature_sensor = TemperatureSensorAccessory::new(1, AccessoryInformation {
-        name: "Pi Temperature Sensor".into(),
-        ..Default::default()
-    })?;
-    let temperature_service = TemperatureSensorService::new(1, 1);
+    // Sensor
+    let device = I2c::new().expect("could not initialize I2c on your RPi");
+    let delay = Delay::new();
+
+    let mut am2320 = Am2320::new(device, delay);
+
+    // HomeKit
+    let mut temperature_sensor = TemperatureSensorAccessory::new(
+        1,
+        AccessoryInformation {
+            name: "Pi Temperature Sensor".into(),
+            ..Default::default()
+        },
+    )?;
+    let mut temperature_service = TemperatureSensorService::new(1, 1);
+    let mut current_temperature = CurrentTemperatureCharacteristic::new(1, 1);
+    current_temperature.on_read(Some(|| {
+        let measurement = am2320.read()?;
+        Ok(Some(measurement.temperature))
+    }));
+
+    temperature_service.current_temperature = current_temperature;
     temperature_sensor.temperature_sensor = temperature_service;
 
     let mut storage = FileStorage::current_dir().await?;
@@ -27,7 +47,7 @@ async fn main() -> Result<()> {
             config.redetermine_local_ip();
             storage.save_config(&config).await?;
             config
-        },
+        }
         Err(_) => {
             let config = Config {
                 pin: Pin::new([1, 1, 1, 2, 2, 3, 3, 3])?,
@@ -38,7 +58,7 @@ async fn main() -> Result<()> {
             };
             storage.save_config(&config).await?;
             config
-        },
+        }
     };
 
     let server = IpServer::new(config, storage).await?;
