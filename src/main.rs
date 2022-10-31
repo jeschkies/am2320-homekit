7b6c43e6ddd0e495dbd1d6a30666288dae010366
Karsten Jeschkies <karsten.jeschkies@grafana.com>
2022-10-31 14:28:21 +0100

    Setup driver and read temperature. (#1)

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,41 @@
-use tokio;
-
+use am2320::Am2320;
 use hap::{
-    accessory::{temperature_sensor::TemperatureSensorAccessory, AccessoryCategory, AccessoryInformation},
-    service::temperature_sensor::TemperatureSensorService,
+    accessory::{
+        temperature_sensor::TemperatureSensorAccessory, AccessoryCategory, AccessoryInformation,
+    },
+    characteristic::{
+        current_temperature::CurrentTemperatureCharacteristic, CharacteristicCallbacks,
+    },
     server::{IpServer, Server},
+    service::temperature_sensor::TemperatureSensorService,
     storage::{FileStorage, Storage},
-    Config,
-    MacAddress,
-    Pin,
-    Result,
+    Config, MacAddress, Pin, Result,
 };
+use rppal::{hal::Delay, i2c::I2c};
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let mut temperature_sensor = TemperatureSensorAccessory::new(1, AccessoryInformation {
-        name: "Pi Temperature Sensor".into(),
-        ..Default::default()
-    })?;
-    let temperature_service = TemperatureSensorService::new(1, 1);
+    // HomeKit
+    let mut temperature_sensor = TemperatureSensorAccessory::new(
+        1,
+        AccessoryInformation {
+            name: "Pi Temperature Sensor".into(),
+            ..Default::default()
+        },
+    )?;
+    let mut temperature_service = TemperatureSensorService::new(1, 1);
+    let mut current_temperature = CurrentTemperatureCharacteristic::new(1, 1);
+    current_temperature.on_read(Some(|| {
+        // Sensor
+        let device = I2c::new().expect("could not initialize I2c on your RPi");
+        let delay = Delay::new();
+
+        let mut am2320 = Am2320::new(device, delay);
+        let measurement = am2320.read().unwrap(); // TODO: use ? and thiserror.
+        Ok(Some(measurement.temperature))
+    }));
+
+    temperature_service.current_temperature = current_temperature;
     temperature_sensor.temperature_sensor = temperature_service;
 
     let mut storage = FileStorage::current_dir().await?;
@@ -27,18 +45,22 @@ async fn main() -> Result<()> {
             config.redetermine_local_ip();
             storage.save_config(&config).await?;
             config
-        },
+        }
         Err(_) => {
+            let address = mac_address::get_mac_address()
+                .unwrap()
+                .map(|a| MacAddress::new(a.bytes()))
+                .unwrap_or(MacAddress::new([10, 20, 30, 40, 50, 60]));
             let config = Config {
                 pin: Pin::new([1, 1, 1, 2, 2, 3, 3, 3])?,
                 name: "Pi Temperature Sensor".into(),
-                device_id: MacAddress::new([10, 20, 30, 40, 50, 60]),
+                device_id: address,
                 category: AccessoryCategory::Sensor,
                 ..Default::default()
             };
             storage.save_config(&config).await?;
             config
-        },
+        }
     };
 
     let server = IpServer::new(config, storage).await?;
